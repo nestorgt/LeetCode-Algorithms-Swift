c022157cefb82c04d02c91382889f6ade66fe3aa
Nestor Garcia <nestor@backbase.com>
2016-03-04 14:34:50 +0100

    create xcode project

---
+++ LeetCode-Algorithms/LeetCode-Algorithms/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  LeetCode-Algorithms
+//
+//  Created by Nestor Garcia on 04/03/16.
+//  Copyright © 2016 nestorgt. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+     
+        return true
+    }
+}
+
