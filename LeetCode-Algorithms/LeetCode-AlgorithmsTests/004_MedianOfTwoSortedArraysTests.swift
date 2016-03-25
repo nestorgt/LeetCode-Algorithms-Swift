9e8aade6f09922a79ee58d63e3a825d7b5366f90
Nestor Garcia <nestor@backbase.com>
2016-03-25 16:33:24 +0100

    test: 003 - Median of Two Sorted Arrays

--- LeetCode-Algorithms/LeetCode-AlgorithmsTests/004_MedianOfTwoSortedArraysTests.swift
+++ LeetCode-Algorithms/LeetCode-AlgorithmsTests/004_MedianOfTwoSortedArraysTests.swift
@@ -12,13 +12,12 @@ import XCTest
 class _004_MedianOfTwoSortedArraysTests: XCTestCase {
     
     func test() {
-        let nums1 = [1,2,3,4,5]
-        let nums2 = [6,7,8,9,10]
-        let expected = Double(3+8/2)
+        let nums1: [Int] = []
+        let nums2: [Int] = [1]
+        let expected: Double = 1.0
         
         let result = _004_MedianOfTwoSortedArrays().findMedianSortedArrays(nums1, nums2)
         
         XCTAssert(result == expected, "result: \(result) - expected: \(expected)")
     }
-
 }
